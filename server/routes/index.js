ee3dc86085eae438cde64b4013782d748159547a
elvece <lvcifferello@gmail.com>
2015-09-03 16:59:30 -0600

    added homepage nav and header

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,13 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-  res.render('index', {title:"Translator"})
+  res.render('index', {title:"Translator"});
 });
 
 router.get('/challenges', function(req, res, next){
-  res.render('challenges', {title: "Challenges!"})
-})
+  res.render('challenges', {title: "Challenges!"});
+});
 
+router.get('/home', function(req, res, next){
+  res.render('home');
+});
 
+router.get('/test', function(req, res, next){
+  res.render('test');
+});
 
 module.exports = router;
