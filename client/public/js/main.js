005f8fa7fb1f6bb1f1644302064bd8cd13152e0d
ZoeAde <zoe.a.adelman@gmail.com>
2015-09-03 16:59:12 -0600

    updating textarea and dropdowns

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,6 +1,13 @@
 // add scripts
 $(document).on('ready', function() {
   console.log('sanity check!');
+$('body').on('click','.option li',function(){
+    var i = $(this).parents('.select').attr('id');
+    var v = $(this).children().text();
+    var o = $(this).attr('id');
+    $('#'+i+' .selected').attr('id',o);
+    $('#'+i+' .selected').text(v);
+});
 
    //practice translation
   $('#translate-form').on('submit', function(event){
