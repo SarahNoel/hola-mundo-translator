0f503f25430beab23c8044ac7a836d22ba205d29
ZoeAde <zoe.a.adelman@gmail.com>
2015-09-08 10:14:49 -0600

    merging progress css and js with main

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -45,10 +45,34 @@ $(".option3 li a").click(function(){
    }); //end submit for practice
 
 
+// Progress Bar
 $("#progress-stats").on("click", function(event) {
      event.preventDefault();
      $("#bar").toggle();
   });
+
+$.getScript('http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js',function(){
+$.getScript('http://cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.0/morris.min.js',function(){
+
+      Morris.Donut({
+        element: 'donut-1',
+        data: [
+         {label: "Correct", value: 70, color: "#578F72"},
+         {label: "Incorrect", value: 30, color:"#61CA80"}
+        ]
+      });
+
+        Morris.Donut({
+        element: 'donut-2',
+        data: [
+         {label: "Correct", value: 10, color: "#3E58E8"},
+         {label: "Incorrect", value: 12, color:"#3EA4E8"}
+        ]
+      });
+});
+});
+
+// End Progress Bar
 
 
 });//end on-ready
