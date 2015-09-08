6ea3f1479399ed203667ede081d0052785a1f2b9
elvece <lvcifferello@gmail.com>
2015-09-08 09:50:12 -0600

    changed header title shadows and content section background color

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,7 +1,7 @@
 // add scripts
 $(document).on('ready', function() {
   console.log('sanity check!');
-  //scrolls to top on page refresh
+  //scrolls to top on page refresh - LUCY ADDED
   $(this).scrollTop(0);
 
    //practice translation
@@ -9,29 +9,29 @@ $(document).on('ready', function() {
    event.preventDefault();
 
    var phrase = $('#to-translate').val().trim();
-   var inputLang = $('#start-lang').val()
-   var outputLang = $('#end-lang').val()
+   var inputLang = $('#start-lang').val();
+   var outputLang = $('#end-lang').val();
 
    var payload = {
       phrase:phrase,
       inputLang:inputLang,
       outputLang:outputLang
-      }
+      };
       console.log(payload);
 
    $('#to-translate').val("");
 
 
    $.post('/api/translate', payload, function(data) {
-      console.log(data)
+      console.log(data);
 
-      $('#result').append('<h4>'+ data.original_text.toLowerCase() +'  is ' + data.translated_text.toLowerCase() + '</h4>')
+      $('#result').append('<h4>'+ data.original_text.toLowerCase() +'  is ' + data.translated_text.toLowerCase() + '</h4>');
       });
    }); //end submit for practice
 
 });//end on-ready
 
-//scrolls nicely to services div when header button clicked
+//scrolls nicely to services div when header button clicked - LUCY ADDED
 $('#find-out-more').on('click', function(e){
   e.preventDefault();
   $('html, body').animate({
