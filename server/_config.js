3141eec856183a78d6d002479a5f49aad25dd388
Sarah Turek <snturek@gmail.com>
2015-11-06 13:20:20 -0700

    pre-heroku

--- server/_config.js
+++ server/_config.js
@@ -1,7 +1,8 @@
 var config = {};
 
 config.mongoURI = {
-  development: 'mongodb://localhost/translator',
+  development: 'mongodb://localhost/node-translator',
+  production: 'mongodb://heroku_4czzxpqf:qrvjln4gvn19c7ca7su59omuvh@ds043324.mongolab.com:43324/heroku_4czzxpqf',
 };
 
 module.exports = config;
